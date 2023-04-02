6205ab8ac199b7dd519a33c58e35511ac9e0504c
sou <ocelot2828@gmail.com>
2023-04-02 20:57:04 +0900

    feat(api posts): add

---
+++ src/app/api/posts/route.ts
@@ -0,0 +1,17 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+import { NextResponse } from 'next/server';
+
+import pageSize from 'libs/pageSize';
+import getAllPosts from 'libs/getAllPosts';
+
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const page = searchParams.get('page');
+  const parsedPage = parseInt(page as string, 10) + 1;
+  const skip = (parsedPage - 1) * pageSize;
+  const limit = parsedPage * pageSize;
+  const posts = await getAllPosts(skip, limit);
+
+  return NextResponse.json(posts);
+}
