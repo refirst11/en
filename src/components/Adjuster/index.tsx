40956d4c934ddc2112ddc2e60ad0ae6d8daabbc0
sou <ocelot2828@gmail.com>
2023-04-04 11:31:55 +0900

    chore(style): brush up

--- src/components/Adjuster/index.tsx
+++ src/components/Adjuster/index.tsx
@@ -1,17 +1,17 @@
 'use client';
 import { ReactNode } from 'react';
 import useLayoutAdjuster from 'hooks/useLayoutAdjuster';
+import { usePathname } from 'next/navigation';
 
 type AnimateProps = {
   children: ReactNode;
-  className?: string;
 };
 
-const Animate = ({ children, className }: AnimateProps): JSX.Element => {
+const Animate = ({ children }: AnimateProps): JSX.Element => {
   const { ref, isToggleClass } = useLayoutAdjuster('layout_center_content', 'layout_top_content');
-
+  const pathname = usePathname();
   return (
-    <main ref={ref} className={`pages_root ${isToggleClass} ${className}`}>
+    <main ref={ref} className={`pages_root ${isToggleClass} ${pathname.includes('/posts/') && 'slug_margin_top'}`}>
       {children}
     </main>
   );
