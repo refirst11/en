72b4d9feca56cec940e48a6f27bf03cdb93dbfaa
sou <ocelot2828@gmail.com>
2023-04-05 18:36:42 +0900

    fix(ui): up

--- src/components/Font/index.tsx
+++ src/components/Font/index.tsx
@@ -3,25 +3,27 @@ import { Playfair_Display, Source_Serif_Pro } from 'next/font/google';
 const other = Playfair_Display({ subsets: ['latin'], weight: '400' });
 const source = Source_Serif_Pro({ subsets: ['latin'], weight: '400' });
 const GlobalFont = (): JSX.Element => {
-  const main = `${other.style.fontFamily}, system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
-  Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, 'Helvetica Neue'`;
   return (
     <style jsx global>
       {`
         html {
-          font-family: ${main};
+          font-family: ${other.style.fontFamily}, system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
+            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, 'Helvetica Neue';
         }
         code {
           font-family: ${source.style.fontFamily}, 'ui-monospace', 'monospace';
         }
         p code {
-          font-family: ${main};
+          font-family: ${other.style.fontFamily}, system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
+            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, 'Helvetica Neue';
         }
         input {
-          font-family: ${main};
+          font-family: ${other.style.fontFamily}, system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
+            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, 'Helvetica Neue';
         }
         textarea {
-          font-family: ${main};
+          font-family: ${other.style.fontFamily}, system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
+            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, 'Helvetica Neue';
         }
       `}
     </style>
