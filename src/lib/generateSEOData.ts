794ed545b4d2906daeeabcdfe725e9e555979703
sou <ocelot2828@gmail.com>
2023-04-06 01:39:45 +0900

    feat(satori): ogp

--- src/lib/generateSEOData.ts
+++ src/lib/generateSEOData.ts
@@ -19,7 +19,11 @@ const generateSEOData = ({ title, subtitle, date }: SeoData) => {
       card: 'summary_large_image',
       title: title,
       description: subtitle,
-      images: `${process.env.PROD_URL}/api/ogp?title=${title}&date=${date}`,
+      images: {
+        url: `${process.env.PROD_URL}/api/ogp?title=${title}&date=${date}`,
+        width: 1200,
+        height: 600,
+      },
       site: '@pull1102',
       creator: '@pull1102',
     },
