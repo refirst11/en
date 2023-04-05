e5fc4902149fba3c3905ce2413abd4fcbfad1322
sou <ocelot2828@gmail.com>
2023-04-05 12:21:29 +0900

    test(revert): test

--- src/components/Font/index.tsx
+++ src/components/Font/index.tsx
@@ -12,8 +12,6 @@ const GlobalFont = (): JSX.Element => {
         }
         code {
           font-family: ${source.style.fontFamily}, 'ui-monospace', 'monospace';
-          font-size: 12.4px;
-          -webkit-text-size-adjust: none;
         }
         p code {
           font-family: ${inter.style.fontFamily}, system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
