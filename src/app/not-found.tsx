88740e6cfa867d6ae2ccaed0abfae71fbf4922b5
S.Watanabe <ocelot2828@gmail.com>
2023-04-08 18:38:33 +0900

    chore(not-found loading): add

---
+++ src/app/not-found.tsx
@@ -0,0 +1,25 @@
+import Link from 'next/link';
+import generateSEOData from 'lib/generateSEOData';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = generateSEOData({
+  title: 'not found 404',
+  subtitle: 'not found url error code is 404',
+  date: 'now',
+});
+
+const NotFound = (): JSX.Element => {
+  return (
+    <>
+      <h1>ClientSide Error status code 404</h1>
+      <p>Request page is not found</p>
+      <dl>
+        <Link href="/" className="redLinks scale">
+          Return to TOP page
+        </Link>
+      </dl>
+    </>
+  );
+};
+
+export default NotFound;
