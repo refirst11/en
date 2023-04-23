e9fcda80f34449da17af9dd64c6382c38474a4fa
S.Watanabe <ocelot2828@gmail.com>
2023-04-24 00:26:56 +0900

    feat(react-page-fitter): update

--- src/components/Adjuster/index.tsx
+++ src/components/Adjuster/index.tsx
@@ -1,23 +1,23 @@
 'use client';
 
-import { ReactNode, useRef } from 'react';
-import { usePathname } from 'next/navigation';
-// import useFitter from 'react-page-fitter';
-import useFitter from 'hooks/useFitter';
+import { ReactNode } from 'react';
+import useFitter, { Main } from 'react-page-fitter';
 
 type AdjusterProps = {
   children: ReactNode;
 };
 
 const Adjuster = ({ children }: AdjusterProps): JSX.Element => {
-  const ref = useRef<HTMLElement>(null);
-  const { isFitted } = useFitter(ref);
-  const isClass = isFitted ? 'layout_center_content' : 'layout_top_content';
-  const pathname = usePathname();
+  const isFit = useFitter();
+
   return (
-    <main ref={ref} className={`pages_root ${isClass} ${pathname.includes('/posts/') && 'slug_margin_top'}`}>
-      {children}
-    </main>
+    <>
+      {isFit !== undefined && (
+        <Main className="pages_root" classFitIn="fit_in" classFitOut="fit_out">
+          {children}
+        </Main>
+      )}
+    </>
   );
 };
 
