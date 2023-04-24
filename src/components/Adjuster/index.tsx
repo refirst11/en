8e6ac3ccd1ff25ca8f69ae7b90746d9be5330232
S.Watanabe <ocelot2828@gmail.com>
2023-04-24 21:09:09 +0900

    chore(fix): up

--- src/components/Adjuster/index.tsx
+++ src/components/Adjuster/index.tsx
@@ -2,6 +2,7 @@
 
 import { ReactNode } from 'react';
 import useFitter, { Main } from 'react-page-fitter';
+import { usePathname } from 'next/navigation';
 
 type AdjusterProps = {
   children: ReactNode;
@@ -9,11 +10,15 @@ type AdjusterProps = {
 
 const Adjuster = ({ children }: AdjusterProps): JSX.Element => {
   const isFit = useFitter();
-
+  const pathname = usePathname();
   return (
     <>
       {isFit !== undefined && (
-        <Main className="pages_root" classFitIn="fit_in" classFitOut="fit_out">
+        <Main
+          className={`pages_root ${pathname.includes('/posts/') && 'slug_margin_top'}`}
+          classFitIn="fit_in"
+          classFitOut="fit_out"
+        >
           {children}
         </Main>
       )}
