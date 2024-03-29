33c78e5b0c6887976736bdc3aaa37d62dc7d20f8
blensen <75829584+su-pull@users.noreply.github.com>
2024-03-30 06:12:11 +0900

    Update Peg.tsx

--- src/components/Peg.tsx
+++ src/components/Peg.tsx
@@ -1,8 +1,8 @@
-'use client';
+'use client'
 
-import { ReactNode } from 'react';
-import { useClix } from 'lib/clix;
-import styles from './styles.module.scss';
+import { ReactNode } from 'react'
+import { useClix } from 'lib/clix
+import styles from './styles.module.scss'
 
 type PegProp = {
   children: ReactNode;
