8f81e816ad858603e06af4b6f5c3bfaa2204dd29
blensen <75829584+su-pull@users.noreply.github.com>
2024-03-30 07:55:10 +0900

    Update Peg.tsx

--- src/components/Peg.tsx
+++ src/components/Peg.tsx
@@ -10,7 +10,7 @@ type PegProp = {
 
 const Peg = ({ children }: PegProp): JSX.Element => {
   return (
-    <p className={useClix([styles.base, styles.init, styles.exit],0.2)}>
+    <p className={useClix([styles.base, styles.init, styles.exit],0.5)}>
       {children}
     </p>
   );
