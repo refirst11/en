d55d8562468a11d5cac2a09a3844a137298061c7
blensen <75829584+su-pull@users.noreply.github.com>
2024-03-29 20:07:17 +0900

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,4 @@
-
-
-import { Peg } from 'components/Peg';
+import { Peg } from 'components/Peg.tsx';
 import { Metadata } from 'next';
 import generateSEOData from 'lib/generateSEOData';
 import ExternalLink from 'components/ExternalLink';
