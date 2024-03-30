35a6da2352a3c630872bdf13ec275ba797b203d4
blensen <75829584+su-pull@users.noreply.github.com>
2024-03-31 08:55:51 +0900

    Update anima.ts

--- src/lib/anima.ts
+++ src/lib/anima.ts
@@ -1,1 +1,87 @@
 
+import { useCallback, useLayoutEffect, useRef, useState } from 'react'
+
+let anchor: HTMLAnchorElement | null
+let firstmount = false
+const useCapture = true
+
+export const useAnimation = (base: string, classes: [string, string, string?], exit?: number) => {
+  const ref = useRef(classes)
+  const [hasDelay, setHasDelay] = useState(false)
+  const [state, setState] = useState('')
+
+  const getClientClassElement = useCallback(() => {
+    const oneClassElement = document.getElementsByClassName(ref.current[0])[0]
+    if (oneClassElement instanceof HTMLElement) return oneClassElement
+    else return null
+  }, [])
+
+  const eventTargetHTMLElement = (e: MouseEvent) => {
+    const clickTarget = e.target
+    if (clickTarget instanceof HTMLElement) return clickTarget
+    else return null
+  }
+
+  const clickHandler = useCallback(
+    (e: MouseEvent) => {
+      const target = eventTargetHTMLElement(e)
+      if (target == null) return
+
+      const anchorElement = target.closest('a')
+      if (anchorElement == null) return
+      if (window.location.href === anchorElement.href) return
+
+      const classElement = getClientClassElement()
+      if (classElement == null) return
+
+      if (!ref.current[1]) return
+      setState(ref.current[1])
+      
+      e.preventDefault()
+      if (typeof exit != 'undefined')
+        setTimeout(() => {
+          setHasDelay(true)
+        }, exit * 1000)
+      anchor = anchorElement
+    },
+    [exit, getClientClassElement]
+  )
+
+  const innerEffect = useCallback(() => {
+    const clickEvent = new MouseEvent('click', {
+      view: window,
+      bubbles: true,
+      cancelable: true
+    })
+    if (!hasDelay) return
+    if (anchor == null) return
+
+    anchor.dispatchEvent(clickEvent)
+    anchor = null
+  }, [hasDelay])
+
+  // ---------- Entry Effect//
+  // ---------- Exits styles. entry the class third of array //
+  useLayoutEffect(() => {
+    innerEffect()
+    document.body.addEventListener('click', clickHandler, useCapture)
+    
+    return () => {
+      document.body.removeEventListener('click', clickHandler, useCapture)
+    }
+  }, [clickHandler, innerEffect])
+
+  // ---------- Initial styles. entry the class second of array //
+  useLayoutEffect(() => {
+    setState(base)
+  
+    firstmount = true
+    const cleanup = ref.current[0]
+
+    return () => {
+      setState(cleanup)
+    }
+  }, [])
+
+  return state !== '' ? state : ref.current[0]
+}
