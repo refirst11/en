de1ac4dc83f1d860dd13daddcfa83f02ec39c62a
blensen <75829584+su-pull@users.noreply.github.com>
2024-03-30 13:33:48 +0900

    Update clix.ts

--- src/lib/clix.ts
+++ src/lib/clix.ts
@@ -33,7 +33,7 @@ export const useClix = (classes: [string, string, string?], exit?: number) => {
       const classElement = getClientClassElement()
       if (classElement == null) return
 
-      if (!ref.current[2]) return
+      // if (!ref.current[2]) return
       setState(ref.current[0] + ' ' + ref.current[2])
       e.preventDefault()
       if (typeof exit != 'undefined')
