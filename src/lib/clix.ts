a119349436adf54c928abd25448b74d96bbb1b60
blensen <75829584+su-pull@users.noreply.github.com>
2024-03-31 11:27:17 +0900

    Update clix.ts

--- src/lib/clix.ts
+++ src/lib/clix.ts
@@ -78,8 +78,12 @@ export const useClix = (classes: [string, string, string?], exit?: number) => {
     const cleanup = ref.current[1]
 
     return () => {
-      if (firstmount) setState(ref.current[0]
-      else setState(cleanup)
+      if (firstmount) {
+        setState(ref.current[0]
+      }
+      else {
+        setState(cleanup)
+      }
     }
   }, [])
 
