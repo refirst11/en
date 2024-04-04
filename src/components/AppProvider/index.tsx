773ce22f4fc2d9d7fd1202620e36a6ae1294748f
blensen <75829584+su-pull@users.noreply.github.com>
2024-04-04 15:13:40 +0900

    Update index.tsx

--- src/components/AppProvider/index.tsx
+++ src/components/AppProvider/index.tsx
@@ -2,6 +2,7 @@ import type { ReactNode } from 'react';
 import { Analytics } from '@vercel/analytics/react';
 import Layout from 'components/Layout';
 import styles from './styles.module.scss';
+import useAnimation from 'lib/clix'
 
 type Provider = {
   children: ReactNode;
@@ -11,7 +12,8 @@ const AppProvider = ({ children }: Provider): JSX.Element => {
   return (
     <Layout>
       <Analytics mode={'production'} />
-      <main className={styles.pages_root}>{children}</main>
+      <main className={useAnimation(
+      styles.pages_root, [styles.init, styles.exit], 0.7}>{children}</main>
     </Layout>
   );
 };
