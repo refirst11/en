43de549ec34aab36e069dfdf940c5fc492539190
S.Watanabe <ocelot2828@gmail.com>
2024-07-04 21:30:42 +0900

    feat(page.tsx): rewrite

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Animation from 'components/Animation';
 import { Metadata } from 'next';
 import generateSEOData from 'lib/generateSEOData';
 import ExternalLink from 'components/ExternalLink';
+import styles from './styles.module.scss';
 
 export const metadata: Metadata = generateSEOData({ title: 'About', subtitle: 'personal page', date: '1994' });
 
@@ -18,17 +19,18 @@ const Page = (): JSX.Element => {
         <ExternalLink href={url}> my projects</ExternalLink> and I want newest and most advanced areas.
       </p>
       <h1>history</h1>
-      <ul>
+      <ul className={styles.list}>
         <li>1994: Born</li>
         <li>2014: Traveled to USA → Canada → Seattle</li>
         <li>2016: Joined PC SHOP - to 2017</li>
         <li>2018: Passed the Japan high school certification</li>
         <li>2019: Learning design - to 2020</li>
         <li>2021: Learning programming and git (developer) to 2022</li>
-        <li>2023: Intern and Creates a React librarys</li>
         <li>
-          2024: Creates <ExternalLink href={fire}>firemotion</ExternalLink> and{' '}
-          <ExternalLink href={type}>typedcssx</ExternalLink>
+          2023: Creates <ExternalLink href={fire}>firemotion</ExternalLink>
+        </li>
+        <li>
+          2024: Creates <ExternalLink href={type}>Typed CSS X</ExternalLink>
         </li>
       </ul>
     </Animation>
