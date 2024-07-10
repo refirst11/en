77f5738160279408eed9701613936eec8c833b29
S.Watanabe <ocelot2828@gmail.com>
2024-07-10 14:28:55 +0900

    feat(fetchCatData.ts): add

---
+++ src/lib/fetchCatData.ts
@@ -0,0 +1,18 @@
+import CatImage from '@/types/CatImage';
+
+const fetchCatData = async (): Promise<CatImage[]> => {
+  const apiKey = process.env.CAT_API_KEY || '';
+  const res = await fetch(`${process.env.CAT_API_URL}?limit=18&breed_ids=beng,abys,soma,siam`, {
+    headers: {
+      'x-api-key': apiKey,
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    },
+    next: { revalidate: 60 * 60 * 12 },
+  });
+
+  const data = await res.json();
+  return data;
+};
+
+export default fetchCatData;
