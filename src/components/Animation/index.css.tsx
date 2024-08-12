ca9941daa87882b630147c974cac7f5e677edc80
S.Watanabe <ocelot2828@gmail.com>
2024-08-12 12:42:08 +0900

    fix(*): padding top bottom is changed main

--- src/components/Animation/index.css.tsx
+++ src/components/Animation/index.css.tsx
@@ -2,19 +2,22 @@
 
 import { ReactNode } from 'react';
 import useFiremotion from 'firemotion';
-import { Style } from 'typedcssx';
+import Style, { max_lg } from 'typedcssx';
 
 type AnimationProps = {
   children: ReactNode;
 };
 
 const Animation = ({ children }: AnimationProps): JSX.Element => {
-  const animate = useFiremotion(styles.base, [styles.entry, styles.exit], 0.2);
+  const animate = useFiremotion(styles.base, [styles.entry, styles.exit], 0.35);
   return <main className={animate}>{children}</main>;
 };
 
 const styles = Style.create({
   base: {
+    [max_lg]: {
+      padding: '120px 0',
+    },
     opacity: 1,
     clipPath: 'inset(0% 0% 0% 0%)',
     transition: 'all 0.5s ease-in-out',
@@ -28,7 +31,7 @@ const styles = Style.create({
   exit: {
     opacity: 0,
     clipPath: 'inset(100% 0% 0% 0%)',
-    transition: 'all 0.17s linear',
+    transition: 'all 0.28s linear',
   },
 });
 
