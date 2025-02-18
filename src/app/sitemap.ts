11e3860cd9cf4c272979ed9c32e7ff1ab9131816
S.Watanabe <ocelot2828@gmail.com>
2025-02-18 12:02:24 +0900

    feat: add sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,15 @@
+import getAllPosts from 'lib/getAllPosts';
 import type { MetadataRoute } from 'next';
 
-export default function sitemap(): MetadataRoute.Sitemap {
-  const url = process.env.PROD_URL || '';
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const url = process.env.VERCEL_URL || '';
+  const posts = await getAllPosts();
+  const currentDate = new Date();
+  const personal: MetadataRoute.Sitemap = posts.map((post) => ({
+    url: `${url}/personal/${post.slug}`,
+    lastModified: currentDate,
+    priority: 0.5,
+  }));
   return [
     {
       url: url,
@@ -13,13 +21,14 @@ export default function sitemap(): MetadataRoute.Sitemap {
       url: url + '/posts',
       lastModified: new Date(),
       changeFrequency: 'monthly',
-      priority: 1.2,
+      priority: 0.5,
     },
     {
-      url: url + '/mail',
+      url: url + '/projects',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.5,
     },
+    ...personal,
   ];
 }
