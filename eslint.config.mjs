7717cfb28c8c30d4a03719a184f29844eda4ec49
S.Watanabe <ocelot2828@gmail.com>
2025-03-17 21:50:53 +0900

    feat: eslint-plugin-stylefix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,11 @@
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
-import objectCss from 'eslint-plugin-object-css';
+import stylefixlint from 'eslint-plugin-stylefix';
 
 const eslintConfig = tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.strict,
-  objectCss.flatConfigs.recommended,
+  stylefixlint.flatConfigs.recommended,
   {
     files: ['**/*.{ts,tsx}'],
     rules: {
