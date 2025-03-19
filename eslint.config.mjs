c19fd5c44123e93d2a088f52e30d9708bd458926
S.Watanabe <ocelot2828@gmail.com>
2025-03-19 20:39:52 +0900

    chore: dependencies update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,11 @@
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
-import stylefixlint from 'eslint-plugin-stylefix';
+import zsslint from 'eslint-plugin-zss-lint';
 
 const eslintConfig = tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.strict,
-  stylefixlint.flatConfigs.recommended,
+  zsslint.flatConfigs.recommended,
   {
     files: ['**/*.{ts,tsx}'],
     rules: {
