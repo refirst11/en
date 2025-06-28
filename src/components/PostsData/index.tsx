5d65fa100f58f700a6680999c0198368e20bc4fd
refirst11 <ocelot2828@gmail.com>
2025-06-28 16:07:25 +0900

    refactor(PostsData): Componentized

--- src/components/PostsData/index.tsx
+++ src/components/PostsData/index.tsx
@@ -1,18 +1,15 @@
-import { Link } from 'next-link-transitions';
 import PostsProps from 'types/PostsProps';
 import { styles } from 'app/listup';
-import { animation } from 'styles/global';
 import { css } from '@plumeria/core';
+import { LinkItem } from 'components/LinkItem';
 
 const PostsData = ({ posts }: PostsProps) => {
   return (
     <div className={css.props(styles.list)}>
       {posts.map(({ slug, title, date }) => (
-        <Link key={slug} href={`/personal/${slug}`} old={css.props(animation.old)} next={css.props(animation.next)}>
-          <span>{title}</span>
-          <div />
-          <span className={css.props(styles.date)}>{date}</span>
-        </Link>
+        <LinkItem key={slug} href={`/personal/${slug}`} date={date}>
+          {title}
+        </LinkItem>
       ))}
     </div>
   );
