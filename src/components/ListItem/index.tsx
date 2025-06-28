772aef8f066ed4d046091be0cecf9f17fbcc4daf
refirst11 <ocelot2828@gmail.com>
2025-06-28 16:05:46 +0900

    feat(ListItem): add component

---
+++ src/components/ListItem/index.tsx
@@ -0,0 +1,23 @@
+import { css } from '@plumeria/core';
+import { styles } from 'app/listup';
+
+export const ListItem = ({
+  children,
+  subti,
+  date,
+  href,
+}: {
+  children: React.ReactNode;
+  subti?: string;
+  date: string;
+  href: string;
+}) => {
+  return (
+    <a className={css.props(styles.link)} href={href}>
+      <span>{children}</span>
+      <span className={css.props(styles.tag)}>{subti}</span>
+      <div />
+      <span className={css.props(styles.date)}>{date}</span>
+    </a>
+  );
+};
