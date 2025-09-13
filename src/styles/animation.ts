4295ac13972d621c6939056fe060128f88025173
refirst11 <ocelot2828@gmail.com>
2025-09-13 20:17:51 +0900

    refactor(animation.ts): has been removed global api

--- src/styles/animation.ts
+++ src/styles/animation.ts
@@ -1,30 +1,10 @@
 import { css } from '@plumeria/core';
 
-const oldTransition = css.keyframes({
-  from: {
-    opacity: 1,
-  },
-  to: {
-    opacity: 1,
-  },
-});
-
-const newTransition = css.keyframes({
-  from: {
-    opacity: 0,
-  },
-  to: {
-    opacity: 1,
-  },
-});
-
-export { oldTransition, newTransition };
-
 export const animation = css.create({
   next: {
-    viewTransitionName: newTransition,
+    viewTransitionName: 'newTransition',
   },
   old: {
-    viewTransitionName: oldTransition,
+    viewTransitionName: 'oldTransition',
   },
 });
