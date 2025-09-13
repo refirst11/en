62ea2e071db9e7ec2c2e9943597f1a8bcb2e6b3f
refirst11 <ocelot2828@gmail.com>
2025-09-13 20:15:38 +0900

    feat(layout.tsx): fix import global.css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import Header from 'components/Header';
 import { ReactNode } from 'react';
 import { Geist, Geist_Mono } from 'next/font/google';
 import '@plumeria/core/stylesheet.css';
-import 'styles/global';
+import 'styles/global.css';
 import Footer from 'components/Footer';
 import { Icon } from 'components/Icon';
 
