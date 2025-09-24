e8ed2d1eadd5f0eb27e2dc4c0c966c29622608cf
refirst11 <ocelot2828@gmail.com>
2025-09-24 17:42:25 +0900

    feat: next-link-transitions update

--- src/components/LinkItem/index.tsx
+++ src/components/LinkItem/index.tsx
@@ -1,16 +1,11 @@
 import { css } from '@plumeria/core';
 import { styles } from 'app/listup';
 import { Link } from 'next-link-transitions';
-import { animation } from 'styles/animation';
+import { transition } from 'styles/animation';
 
 export const LinkItem = ({ children, date, href }: { children: React.ReactNode; date: string; href: string }) => {
   return (
-    <Link
-      className={css.props(styles.link)}
-      href={href}
-      old={css.props(animation.old)}
-      next={css.props(animation.next)}
-    >
+    <Link className={css.props(styles.link)} href={href} viewTransitionName={css.props(transition.name)}>
       <span>{children}</span>
       <div />
       <span className={css.props(styles.date)}>{date}</span>
