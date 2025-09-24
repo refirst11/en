92244035c882cd63e3d3d0c391d0b75023343656
refirst11 <ocelot2828@gmail.com>
2025-09-24 17:42:09 +0900

    feat: keyframes and viewTransition

--- src/styles/animation.ts
+++ src/styles/animation.ts
@@ -1,10 +1,28 @@
 import { css } from '@plumeria/core';
 
-export const animation = css.create({
-  next: {
-    viewTransitionName: 'newTransition',
-  },
+const fadeOut = css.keyframes({
+  from: { opacity: 1 },
+  to: { opacity: 1 },
+});
+
+const fadeIn = css.keyframes({
+  from: { opacity: 0 },
+  to: { opacity: 1 },
+});
+
+const longCrossFade = css.viewTransition({
   old: {
-    viewTransitionName: 'oldTransition',
+    animation: fadeOut,
+    animationDuration: '1.4s',
+  },
+  new: {
+    animation: fadeIn,
+    animationDuration: '1.4s',
+  },
+});
+
+export const transition = css.create({
+  name: {
+    viewTransitionName: longCrossFade,
   },
 });
