53fb81a42d5fdde681e6314dd83df674f80f5739
refirst11 <ocelot2828@gmail.com>
2025-11-25 11:13:19 +0900

    feat(components/Icon): ui user interaction update

--- src/components/Icon/index.tsx
+++ src/components/Icon/index.tsx
@@ -2,8 +2,11 @@
 
 import { usePathname } from 'next/navigation';
 import { css } from '@plumeria/core';
-import { ps } from 'lib/pseudos';
-import { rotation } from './animation';
+import { Link } from 'next-link-transitions';
+import { breakpoints } from 'lib/mediaQuery';
+import { useState } from 'react';
+import { scaling } from './animation';
+import { transition } from 'styles/animation';
 
 const styles = css.create({
   top: {
@@ -12,31 +15,84 @@ const styles = css.create({
     marginTop: -130,
     transition: '0.8s ease',
   },
+  article: {
+    top: -28,
+    left: -50,
+    scale: 0.56,
+    [breakpoints.md]: {
+      top: -45,
+      left: -45,
+      scale: 0.32,
+    },
+  },
+  sentence: {
+    display: 'block',
+    marginTop: -54,
+    marginLeft: -40,
+    color: 'initial',
+    cursor: 'help',
+    [breakpoints.md]: {
+      marginTop: -80,
+      marginLeft: 140,
+      whiteSpace: 'nowrap',
+    },
+  },
+  opacity: {
+    display: 'none',
+    opacity: 0,
+  },
+  animation: {
+    animationName: scaling,
+    animationDuration: '2s',
+  },
   image: {
     position: 'absolute',
-    top: 40,
+    top: 26,
     left: 0,
-    zIndex: -1,
+    zIndex: 1,
     display: 'block',
     borderRadius: '50%',
     transition: '0.8s ease',
     viewTransitionName: 'none',
-    [ps.hover]: {
-      animationName: rotation,
-      animationDuration: '0.5s',
-    },
   },
 });
 
 export const Icon = () => {
   const pathname = usePathname();
+  const [state, setState] = useState(false);
+
+  const handleClick = (e: MouseEvent) => {
+    e.preventDefault();
+    // e.stopPropagation();
+
+    if (state) {
+      return;
+    }
+
+    setState(true);
+  };
+
   return (
-    <img
-      className={css.props(styles.image, pathname === '/' && styles.top)}
-      width={160}
-      height={160}
-      src="/icon.png"
-      alt="static"
-    />
+    <Link href="/" viewTransitionName={css.props(transition.name)}>
+      <img
+        className={css.props(
+          styles.image,
+          state && styles.animation,
+          pathname === '/' && styles.top,
+          pathname.includes('personal/') && styles.article,
+        )}
+        width={160}
+        height={160}
+        src="/icon.png"
+        alt="static"
+        onAnimationEnd={() => setState(false)}
+      />
+      <span
+        onClick={(e) => handleClick(e as unknown as MouseEvent)}
+        className={css.props(styles.sentence, pathname !== '/' && styles.opacity)}
+      >
+        hyper happy {state ? '!' : '?'}
+      </span>
+    </Link>
   );
 };
