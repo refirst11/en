308ebf33e1205755db6146d5feea688682d4acc6
refirst11 <ocelot2828@gmail.com>
2026-01-02 22:12:10 +0900

    chore: next.config explicit caching

--- next.config.ts
+++ next.config.ts
@@ -26,6 +26,11 @@ const securityHeaders = [
     key: 'Referrer-Policy',
     value: 'strict-origin-when-cross-origin',
   },
+
+  {
+    key: 'Cache-Control',
+    value: 'public, max-age=2592000, s-maxage=31536000, stale-while-revalidate=86400',
+  },
 ];
 
 const nextConfig: NextConfig = {
@@ -34,6 +39,15 @@ const nextConfig: NextConfig = {
       {
         source: '/(.*)',
         headers: securityHeaders,
+      },
+      {
+        source: '/_next/static/(.*)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
+        ],
       },
     ];
   },
