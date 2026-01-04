65ee2dd30eeb301f07bf3e853f3c98d6095ab228
refirst11 <ocelot2828@gmail.com>
2026-01-04 17:49:39 +0900

    chore: next.config explicit static caching

--- next.config.ts
+++ next.config.ts
@@ -35,6 +35,15 @@ const nextConfig: NextConfig = {
         source: '/(.*)',
         headers: securityHeaders,
       },
+      {
+        source: '/_next/static/(.*)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
+        ],
+      },
     ];
   },
 };
