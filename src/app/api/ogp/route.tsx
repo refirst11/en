05bbe3c8b8c0492ad08525a07cbf9ba0fefb2de4
refirst11 <ocelot2828@gmail.com>
2026-02-13 21:46:27 +0900

    chore(app/api/ogp): route.tsx update

--- src/app/api/ogp/route.tsx
+++ src/app/api/ogp/route.tsx
@@ -10,55 +10,52 @@ export async function GET(req: NextRequest) {
   const url = process.env.PROD_URL;
 
   return new ImageResponse(
-    (
+    <div
+      style={{
+        display: 'flex',
+        width: '100%',
+        height: '100%',
+        background: '#fffffe',
+      }}
+    >
       <div
         style={{
           display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
           width: '100%',
           height: '100%',
-          background: '#fffffe',
+          padding: '200px',
+          fontSize: '24px',
+          color: 'rgb(150 150 150)',
+          textAlign: 'center',
         }}
       >
+        <img
+          src={url + '/icon.png'}
+          style={{
+            position: 'absolute',
+            left: '100px',
+            width: '200px',
+            height: '200px',
+            alignItems: 'center',
+            borderRadius: '50%',
+            display: 'flex',
+            boxShadow: '0 0 4px -1px var(--color-shadow)',
+          }}
+        />
+        {title}
         <div
           style={{
+            position: 'absolute',
+            bottom: '20px',
             display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            width: '100%',
-            height: '100%',
-            padding: '200px',
             fontSize: '24px',
-            color: 'rgb(150 150 150)',
-            textAlign: 'center',
           }}
         >
-          <img
-            src={url + '/alum.png'}
-            style={{
-              position: 'absolute',
-              top: -60,
-              right: -40,
-              width: '200px',
-              height: '200px',
-              borderRadius: '8px',
-              boxShadow: '0 0 4px -1px var(--color-shadow)',
-              transform: 'rotate(-40deg) scaleX(-1)',
-            }}
-          />
-          {title}
-          <div
-            style={{
-              position: 'absolute',
-              right: '40px',
-              bottom: '20px',
-              display: 'flex',
-              fontSize: '24px',
-            }}
-          >
-            {date}
-          </div>
+          {date}
         </div>
       </div>
-    )
+    </div>,
   );
 }
