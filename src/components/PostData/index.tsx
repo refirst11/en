ce946d4bc8e1e2eb37487fb19b1ab101c4d8485c
refirst11 <ocelot2828@gmail.com>
2026-04-05 16:52:41 +0900

    refactor(app/components/PostData): react plumeria syntax

--- src/components/PostData/index.tsx
+++ src/components/PostData/index.tsx
@@ -1,13 +1,12 @@
-import React from 'react';
 import PostProps from 'types/PostProps';
 import { Link } from 'next-link-transitions';
 import { styles } from './styles';
 import { transition } from 'styles/animation';
-import * as css from "@plumeria/core";
+import * as css from '@plumeria/core';
 
 const ReturnLink = () => {
   return (
-    <Link className={css.use(styles.back)} href="/personal" viewTransitionName={css.use(transition.name)}>
+    <Link styleName={styles.back} href="/personal" viewTransitionName={css.use(transition.name)}>
       back
     </Link>
   );
@@ -15,10 +14,10 @@ const ReturnLink = () => {
 
 const PostData = ({ post }: PostProps) => {
   return (
-    <div className={css.use(styles.post)}>
-      <div className={css.use(styles.spacing)}>
+    <div styleName={styles.post}>
+      <div styleName={styles.spacing}>
         <ReturnLink />
-        <h1 className={css.use(styles.color_h1)}>{post.title}</h1>
+        <h1 styleName={styles.color_h1}>{post.title}</h1>
         <div dangerouslySetInnerHTML={{ __html: post.content }} />
       </div>
     </div>
