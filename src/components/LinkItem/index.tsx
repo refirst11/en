8c6a7fa6168f5f196c4f875b5fa6f269829243a0
refirst11 <ocelot2828@gmail.com>
2026-04-05 16:52:28 +0900

    refactor(app/components/LinkItem): react plumeria syntax

--- src/components/LinkItem/index.tsx
+++ src/components/LinkItem/index.tsx
@@ -1,14 +1,14 @@
-import * as css from "@plumeria/core";
+import * as css from '@plumeria/core';
 import { styles } from 'app/listup';
 import { Link } from 'next-link-transitions';
 import { transition } from 'styles/animation';
 
 export const LinkItem = ({ children, date, href }: { children: React.ReactNode; date: string; href: string }) => {
   return (
-    <Link className={css.use(styles.link)} href={href} viewTransitionName={css.use(transition.name)}>
+    <Link styleName={styles.link} href={href} viewTransitionName={css.use(transition.name)}>
       <span>{children}</span>
       <div />
-      <span className={css.use(styles.date)}>{date}</span>
+      <span styleName={styles.date}>{date}</span>
     </Link>
   );
 };
