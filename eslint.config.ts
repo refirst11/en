faf70d314536f23bd8285cdf1438179dfb824479
refirst11 <ocelot2828@gmail.com>
2026-05-25 08:04:58 +0900

    chore: update eslint.config.ts

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,16 +1,11 @@
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import { defineConfig } from 'eslint/config';
-import { plumeria } from '@plumeria/eslint-plugin';
+import plumeria from '@plumeria/eslint-plugin';
 
-export default defineConfig(
-  eslint.configs.recommended,
-  tseslint.configs.recommended,
-  plumeria.flatConfigs.recommended,
-  {
-    files: ['**/*.{ts,tsx}'],
-    rules: {
-      '@typescript-eslint/no-unused-expressions': 'off',
-    },
-  }
-);
+export default defineConfig(eslint.configs.recommended, tseslint.configs.recommended, plumeria.configs.recommended, {
+  files: ['**/*.{ts,tsx}'],
+  rules: {
+    '@typescript-eslint/no-unused-expressions': 'off',
+  },
+});
